debaa29ed5b9f793fac561e7a3c60c281739906b
marcustib <mtgn@cin.ufpe.br>
2018-05-14 23:17:47 -0300

    #9

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'equipe' => 'equipe#index'
   get 'informa' => 'informa#index'
   get 'home'=>  'home#index'
-  
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
