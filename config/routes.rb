51c11ff0bea6c5b08d92771f565175ef83c5636b
Paulo Marques <pemsm@cin.ufpe.br>
2018-05-16 00:49:22 +0000

    adicao pagina de estados #9

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'equipe' => 'equipe#index'
   get 'informa' => 'informa#index'
   get 'home'=>  'home#index'
+  get '/estados', to: 'home#estados'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
